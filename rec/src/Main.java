b17bf07ffa3ae7aee788d6cd9365f43c03d80f91
vlasabo <104648758+vlasabo@users.noreply.github.com>
2022-05-11 15:43:45 +0300

    Рекурсивный размер

--- rec/src/Main.java
+++ rec/src/Main.java
@@ -6,12 +6,17 @@ public class Main {
         for (int i = 1; i < 7; i++) {
             arr.add(i);
         }
-        int rekSum=GetRekursSumm(arr,0);
+        int rekSum=GetRecursSumm((ArrayList) arr.clone());
+        int sizeArrayList=GetRecursSize((ArrayList) arr.clone());
         System.out.println(rekSum);
+        System.out.println(sizeArrayList);
     }
 
-    private static int GetRekursSumm(ArrayList arrList, int sum){
+    private static int GetRecursSumm(ArrayList arrList){
+        if (arrList.size()==0)
+            return 0;
 
+        int sum=0;
         if (arrList.size()==1) {
             return (int) arrList.get(0);
         }
@@ -19,8 +24,20 @@ public class Main {
             sum=(int) arrList.get(0);
             arrList.remove(0);
 
-            return sum+GetRekursSumm(arrList,sum);
+            return sum+GetRecursSumm(arrList);
         }
 
     }
+
+
+    private static int GetRecursSize(ArrayList arrList){
+    int sizeArrayList=0;
+    if (arrList.isEmpty())
+        return 0;
+    else
+        arrList.remove(0);
+        return GetRecursSize(arrList)+1;
+    }
+
+
 }
