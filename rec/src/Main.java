2f3d99ad793840eddee633ea36b61c40748935ad
vlasabo <104648758+vlasabo@users.noreply.github.com>
2022-05-11 14:43:39 +0300

    Рекурсивная сумма

--- rec/src/Main.java
+++ rec/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        var arr = new ArrayList <>();
+        for (int i = 1; i < 7; i++) {
+            arr.add(i);
+        }
+        int rekSum=GetRekursSumm(arr,0);
+        System.out.println(rekSum);
+    }
+
+    private static int GetRekursSumm(ArrayList arrList, int sum){
+
+        if (arrList.size()==1) {
+            return (int) arrList.get(0);
+        }
+        else  {
+            sum=(int) arrList.get(0);
+            arrList.remove(0);
+
+            return sum+GetRekursSumm(arrList,sum);
+        }
 
     }
 }
